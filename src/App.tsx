828dcff7c8f429ddeb688e6e1b70bf301f39a10d
Arad5 <arad5@outlook.com>
2022-08-23 04:36:00 +0430

    game states created & starting game, next question, previous question implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,67 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 //api
 import fetchQuestions from "./API/fetchQuestions";
 
 //question difficulity enum
-import { Difficulty } from "./types/Question";
+import QuestionType, { Difficulty } from "./types/Question";
 
 // components
 import Question from "./components/Question";
 
 const App = () => {
+    const [questions, setQuestions] = useState<QuestionType[]>([]);
+    const [questionsQuantity, setQuestionsQuantity] = useState<number>(10);
+    const [difficulity, setDifficulity] = useState<Difficulty>(
+        Difficulty.MEDIUM,
+    );
+    const [isStarted, setIsStarted] = useState<boolean>(false);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState<number>(0);
+    const setQuestionsState = async () => {
+        setQuestions(await fetchQuestions(questionsQuantity, difficulity));
+        setIsLoading(false);
+    };
+
     useEffect(() => {
-        const getQuestions = async () => {
-            console.log(await fetchQuestions());
-        };
-        getQuestions();
-    }, []);
+        console.log(questions);
+    }, [questions]);
+
+    const handleStart = () => {
+        setQuestionsState();
+        setIsStarted(true);
+        setIsLoading(true);
+    };
+
+    useEffect(() => {
+        console.log(currentQuestionIndex);
+    }, [currentQuestionIndex]);
 
     return (
         <div className="App">
             <h1>Arad quiz</h1>
-            <button>start</button>
-            <Question />
-            <button>next</button>
+            {!isStarted && <button onClick={handleStart}>start</button>}
+            {questions.length > 0 && (
+                <Question {...questions[currentQuestionIndex]} />
+            )}
+            {isLoading && <div>loading ...</div>}
+            {currentQuestionIndex <= questions.length - 1 &&
+                currentQuestionIndex !== 0 && (
+                    <button
+                        onClick={() =>
+                            setCurrentQuestionIndex((curr) => curr - 1)
+                        }
+                    >
+                        previous
+                    </button>
+                )}
+            {currentQuestionIndex <= questions.length - 2 && (
+                <button
+                    onClick={() => setCurrentQuestionIndex((curr) => curr + 1)}
+                >
+                    next
+                </button>
+            )}
         </div>
     );
 };
--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from "react";
 
-const Question = (): JSX.Element => {
-    return <div>Questions</div>;
+//prop type
+import QuestionType from "../types/Question";
+
+const Question = ({ question }: QuestionType) => {
+    return <div>{question}</div>;
 };
 
 export default Question;
